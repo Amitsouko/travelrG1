5f8766fb8cd000b0595a28652dcfa2e3798e7eb5
fpumir <fpumir@terredeweb.fr>
2013-01-25 10:00:57 +0100

    locationPictures implement 1.0
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -10,6 +10,10 @@ class App_controller{
     $location=$App->locationDetails();
     F3::set('location',$location);
     
+    $pictures=$App->locationPictures($location->id);
+    
+    
+    
     //F3::set('location',App::instance()->locationDetails(););
     
     echo Views::instance()->render('travelr.html');
