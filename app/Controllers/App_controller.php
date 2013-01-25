05f1d0ba467ff343fe0b027d83a62993a0d2b2e3
fpumir <fpumir@terredeweb.fr>
2013-01-25 10:50:45 +0100

    indexation implement 1.0
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Controllers/App_controller.php
+++ app/Controllers/App_controller.php
@@ -6,13 +6,25 @@ class App_controller{
  }
  
  function home(){
+    #récupération de la location
     $App=new App();
     $location=$App->locationDetails();
     F3::set('location',$location);
     
+    #récupération des images de la location
     $pictures=$App->locationPictures($location->id);
     $json=Views::instance()->toJson($pictures,array('image'=>'src'));
     F3::set('pictures',$json);
+    
+    $next=$App->next($location->id);
+    $prev=$App->prev($location->id);
+    
+    $prev=$prev?$prev[0]['id'].'-'.$prev[0]['title']:'';
+    $next=$next?$next[0]['id'].'-'.$next[0]['title']:'';
+
+    F3::set('prev',$prev);
+    F3::set('next',$next);
+    
     
     //F3::set('location',App::instance()->locationDetails(););
     
