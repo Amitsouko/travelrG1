303bb581a8d4732a2bbd6cdc268507d7462d0134
fpumir <fpumir@terredeweb.fr>
2013-01-25 10:00:57 +0100

    locationPictures implement 1.0
    Signed-off-by: fpumir <fpumir@terredeweb.fr>

--- app/Models/App.php
+++ app/Models/App.php
@@ -8,7 +8,12 @@ class App extends Prefab{
     //return F3::get('dB')->exec('select * from location limit 1');
     $location=new DB\SQL\Mapper(F3::get('dB'),'location');
     return $location->load();
-      
+  }
+  
+  function locationPictures($idLocation){
+    //F3::get('dB')->exec('select * from pictures where idLocation='.$idLocation);
+    $pictures=new DB\SQL\Mapper(F3::get('dB'),'pictures');
+    return $pictures->find(array('idLocation=?',$idLocation));
   }
   
 
